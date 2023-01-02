dbd402da80b67b9e0675456fc856f06fe5daa2d8
Amit T. Dorle <Amitdorle111@gmail.com>
2023-01-02 14:04:48 +0530

    practice changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Amit is great
           </Link>
         </div>
       </div>
